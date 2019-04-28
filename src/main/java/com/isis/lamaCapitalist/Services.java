26d57933f8ee74f3935c91e99607eedcddd96eec
Mazel <lucasmazelpro@gmail.com>
2019-04-28 17:38:22 +0200

    dev : prise en compte des users

--- src/main/java/com/isis/lamaCapitalist/Services.java
+++ src/main/java/com/isis/lamaCapitalist/Services.java
@@ -2,12 +2,16 @@ package com.isis.lamaCapitalist;
 
 
 import generated.World;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.text.Document;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -38,7 +42,55 @@ public class Services {
         return null;
     }
     
+    public World readWorldFromXml(String pseudo){      
+        try {
+            InputStream input = new FileInputStream(getWorldFileName(pseudo));
+            World w;
+            JAXBContext jaxbContext;
+            try {
+                jaxbContext = JAXBContext.newInstance(World.class);
+                Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+                w = (World) unmarshaller.unmarshal(input);
+                return w;
+            } catch (JAXBException ex) {
+                Logger.getLogger(Services.class.getName()).log(Level.SEVERE, null, ex);
+                return null;
+            }
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Services.class.getName()).log(Level.WARNING, "Le monde de " +pseudo+" n'existe pas: création");
+            World w = readWorldFromXml();
+            saveWordlToXml(w,pseudo);   
+            return w;            
+        } 
+    }
+    
     public void saveWordlToXml(World world){
         //OutputStream output = new FileOutputStream(file);
+        JAXBContext jaxbContext;
+        try {
+            jaxbContext = JAXBContext.newInstance(World.class);
+            Marshaller marshaller = jaxbContext.createMarshaller();
+            marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+            marshaller.marshal(world, new File("world.xml"));
+        } catch (JAXBException ex) {
+            Logger.getLogger(Services.class.getName()).log(Level.SEVERE, null, ex);
+        }        
     }
+    
+     public void saveWordlToXml(World world,String pseudo){
+         //OutputStream output = new FileOutputStream(file);
+        JAXBContext jaxbContext;
+        try {
+            jaxbContext = JAXBContext.newInstance(World.class);
+            Marshaller marshaller = jaxbContext.createMarshaller();
+            marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+            marshaller.marshal(world, new File(getWorldFileName(pseudo)));
+        } catch (JAXBException ex) {
+            Logger.getLogger(Services.class.getName()).log(Level.SEVERE, null, ex);
+        }        
+    }
+     
+     private String getWorldFileName(String pseudo){
+         return "./Worlds/world_" + pseudo + ".xml";
+     }
 }
--- src/main/java/com/isis/lamaCapitalist/Webservice.java
+++ src/main/java/com/isis/lamaCapitalist/Webservice.java
@@ -15,9 +15,11 @@ package com.isis.lamaCapitalist;
 
 
 
+import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -28,10 +30,21 @@ public class Webservice {
         services = new Services();
     }
     
-    @GET
+    /*@GET
     @Path("world")
     @Produces(MediaType.APPLICATION_XML)
     public Response getWorld() {
         return Response.ok(services.readWorldFromXml()).build();
+    }*/
+    @GET
+    @Path("world")
+    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    public Response getXml(@Context HttpServletRequest request) {
+        String username = request.getHeader("X-user");
+        if(username!=null){
+            return Response.ok(services.readWorldFromXml(username)).build();
+        } else {
+           return Response.ok(services.readWorldFromXml()).build();
+        }
     }
 }
